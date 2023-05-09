351c0f78501f0b9ff70aa2dde4d54d78ec782d15
artur-panichev <artur.panichev152@gmail.com>
2023-05-09 09:18:30 +0300

    level data example

--- src/pages/home/homePage.py
+++ src/pages/home/homePage.py
@@ -4,8 +4,8 @@ import os;
 class HomePage:
     def __init__(self):
         dirname = os.path.dirname(__file__);
-        with open(f'{dirname}/style.css') as f:
-            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+        with open(f'{dirname}/style.css') as file:
+            st.markdown(f'<style>{file.read()}</style>', unsafe_allow_html=True)
         
         st.header('Home page');
         for i in range(1,5):
