95756eb4f84719624c09351343d6893f9b77a72a
artur-panichev <114880871+artur-panichev@users.noreply.github.com>
2023-06-15 07:00:23 +0300

    Delete src directory

--- src/utils/mongodb.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/");
-
-db = client["eesti-learn"];
-table = db["users"];
-
-for x in table.find({"login": "artur"}):
-    print(x)
